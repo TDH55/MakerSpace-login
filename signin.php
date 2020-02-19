4821e01465e5302744ef4b518c2f72cf9aefa839
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-19 00:19:56 -0600

    remove echo

--- signin.php
+++ signin.php
@@ -5,21 +5,19 @@ require "mysql_login.php";
 $id = $_POST['id'];
 $stmt = $mysqli->prepare("insert into sign_ins (id, time) values (?, now())"); //query to insert username and password into users database
     if(!$stmt){ //error message
-        echo "1";
         $_SESSION['error'] = true;
         printf("Query Prep Failed: %s\n", $mysqli->error);
-        header("Location: http://172.27.16.208/MakerSpace-login/home.php");
+        header("Location: home.php");
         exit;
     }
     $stmt->bind_param('s', $id); //set the insert parameters to the username and hashed pssword
     if(!$stmt->execute()){ //go to error page on failure
         $_SESSION['error'] = true;
-        echo "2";
         printf("Query Prep Failed: %s\n", $mysqli->error);
-        header("Location: http://172.27.16.208/MakerSpace-login/home.php");
+        header("Location: home.php");
         exit;
     }
 $stmt->close();
-header("Locaton: http://172.27.16.208/MakerSpace-login/home.php");
+header("Locaton: home.php");
 exit;
 ?>
