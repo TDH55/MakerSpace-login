32b88c29ba372fecc5c0421760868f9d487b27da
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-18 18:16:00 -0600

    fixed error

--- signin.php
+++ signin.php
@@ -5,12 +5,15 @@ require "mysql_login.php";
 $id = $_POST['id'];
 $stmt = $mysqli->prepare("insert into makerspace_sign_in (id, time) values (?, now()"); //query to insert username and password into users database
     if(!$stmt){ //error message
+        echo "1";
         $_SESSION['error'] = true;
         header("Location: home.php");
         exit;
+    }
     $stmt->bind_param('s', $id); //set the insert parameters to the username and hashed pssword
     if(!$stmt->execute()){ //go to error page on failure
         $_SESSION['error'] = true;
+        echo "2";
         header("Location: home.php");
         exit;
     }
