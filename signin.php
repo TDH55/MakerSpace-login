293d86735cdbe34730842d2555b6ac766572ff47
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-19 00:25:11 -0600

    fixed typo

--- signin.php
+++ signin.php
@@ -8,20 +8,18 @@ $id = $_POST['id'];
 $stmt = $mysqli->prepare("insert into sign_ins (id, time) values (?, now())"); //query to insert username and password into users database
     if(!$stmt){ //error message
         $_SESSION['error'] = true;
-        echo "1";
         printf("Query Prep Failed: %s\n", $mysqli->error);
-        //header("Location: home.php");
+        header("Location: home.php");
         exit;
     }
     $stmt->bind_param('s', $id); //set the insert parameters to the username and hashed pssword
     if(!$stmt->execute()){ //go to error page on failure
         $_SESSION['error'] = true;
-        echo "2";
         printf("Query Prep Failed: %s\n", $mysqli->error);
-        //header("Location: home.php");
+        header("Location: home.php");
         exit;
     }
 $stmt->close();
-header("Locaton: home.php");
+header("Location: home.php");
 exit;
 ?>
