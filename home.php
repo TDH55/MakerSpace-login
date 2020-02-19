15592e7d242e5bce74f4940164fb947f2c0fd3f0
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-18 18:14:13 -0600

    mysql script

--- home.php
+++ home.php
@@ -5,11 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sign in</title>
+    <?php
+    session_start();
+    ?>
 </head>
 <body>
     <form action='signin.php' method='POST' autocomplete="off">
         <label> id: <input type='text' name='id' autofocus="autofocus"/> <br/> </label>
         <input type='submit' value='submit'/>
     </form>
+    <?php
+    if($_SESSION['error']){
+        echo "The last user was not signed in";
+    }
+    ?>
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -1,7 +1,20 @@
 <?php
+session_start();
 require "mysql_login.php";
-//script to upload to database
 
+$id = $_POST['id'];
+$stmt = $mysqli->prepare("insert into makerspace_sign_in (id, time) values (?, now()"); //query to insert username and password into users database
+    if(!$stmt){ //error message
+        $_SESSION['error'] = true;
+        header("Location: home.php");
+        exit;
+    $stmt->bind_param('s', $id); //set the insert parameters to the username and hashed pssword
+    if(!$stmt->execute()){ //go to error page on failure
+        $_SESSION['error'] = true;
+        header("Location: home.php");
+        exit;
+    }
+$stmt->close();
 header("Locaiton: home.php");
 exit;
 ?>
