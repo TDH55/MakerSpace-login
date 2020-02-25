3e90a13cf140cf1b1ed548a12c3adf45dec29c04
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-25 17:43:31 -0600

    combine files

--- emailfile.py
+++ emailfile.py
@@ -1,9 +1,42 @@
+#download code
+import MySQLdb
+import sys
+import csv
+from datetime import date
+#email code
 import smtplib
 from email.mime.multipart import MIMEMultipart 
 from email.mime.text import MIMEText 
 from email.mime.base import MIMEBase 
 from email import encoders 
 from datetime import date
+
+#download code
+db = MySQLdb.connect(
+    host = 'localhost',
+    user = 'jubelmakerspace',
+    passwd = 'MakerTech138',
+    database = 'makerspace_sign_in'
+)
+today = date.today()
+d = today.strftime("%d-%m-%y")
+filename = "/home/pi/Documents/signins_" +str(today)+".csv"
+# print(filename)
+
+
+QUERY='SELECT * FROM sign_ins;'
+
+
+# print(db)
+cur = db.cursor()
+cur.execute(QUERY)
+result = cur.fetchall()
+
+c = csv.writer(open(filename, 'w'))
+for x in result:
+    # print(x)
+    c.writerow(x)
+#email code
 
 fromaddr = "jubelmakerspace@gmail.com"
 toaddr = "jubelmakerspace@gmail.com"
@@ -29,9 +62,9 @@ body = ""
 msg.attach(MIMEText(body, 'plain')) 
 
 # open the file to be sent  
-today = date.today()
-d = today.strftime("%d-%m-%y")
-filename = "signins_" +str(today)+".csv"
+# today = date.today()
+# d = today.strftime("%d-%m-%y")
+# filename = "signins_" +str(today)+".csv"
 
 attachment = open("/home/pi/Documents/" + filename, "rb") 
 
