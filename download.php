a4e257fba8127a59462b590b850c2ce2522407be
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-25 16:22:04 -0600

    download test

--- download.php
+++ download.php
@@ -30,53 +30,53 @@ if (count($sign_ins) > 0) {
         fputcsv($output, $row);
     }
 }
-//email file here
-$to = 'jubelmakerspace@gmail.com';
-$from = 'jubelmakerspace@gmail.com';
-$fromName = 'sign in system';
-$day = date('d-m-Y');
-$subject = 'sign in sheet as of '.$day;
+// //email file here
+// $to = 'jubelmakerspace@gmail.com';
+// $from = 'jubelmakerspace@gmail.com';
+// $fromName = 'sign in system';
+// $day = date('d-m-Y');
+// $subject = 'sign in sheet as of '.$day;
 
-$file = "/home/pi/Downloads/".$day.".csv";
+// $file = "/home/pi/Downloads/".$day.".csv";
 
-$headers = "From: $fromName"." <".$from.">";
+// $headers = "From: $fromName"." <".$from.">";
 
-$message = "";
+// $message = "";
 
-//boundary 
-$semi_rand = md5(time()); 
-$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
-$htmlContent = '';
+// //boundary 
+// $semi_rand = md5(time()); 
+// $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+// $htmlContent = '';
 
-//headers for attachment 
-$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+// //headers for attachment 
+// $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
 
-//multipart boundary 
-$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
+// //multipart boundary 
+// $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+// "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
 
-//preparing attachment
-if(!empty($file) > 0){
-    if(is_file($file)){
-        $message .= "--{$mime_boundary}\n";
-        $fp =    @fopen($file,"rb");
-        $data =  @fread($fp,filesize($file));
+// //preparing attachment
+// if(!empty($file) > 0){
+//     if(is_file($file)){
+//         $message .= "--{$mime_boundary}\n";
+//         $fp =    @fopen($file,"rb");
+//         $data =  @fread($fp,filesize($file));
 
-        @fclose($fp);
-        $data = chunk_split(base64_encode($data));
-        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
-        "Content-Description: ".basename($file)."\n" .
-        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
-        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-    }
-}
-$message .= "--{$mime_boundary}--";
-$returnpath = "-f" . $from;
+//         @fclose($fp);
+//         $data = chunk_split(base64_encode($data));
+//         $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
+//         "Content-Description: ".basename($file)."\n" .
+//         "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
+//         "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+//     }
+// }
+// $message .= "--{$mime_boundary}--";
+// $returnpath = "-f" . $from;
 
-$mail = @mail($to, $subject, $message, $headers, $returnpath); 
+// $mail = @mail($to, $subject, $message, $headers, $returnpath); 
 
 
-header("Location: home.php");
-exit;
+// header("Location: home.php");
+// exit;
 
 ?>
