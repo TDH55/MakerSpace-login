9898efed37fc6303442e5565f68595649c6d27b6
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-25 17:02:44 -0600

    writecsv

--- download.py
+++ download.py
@@ -1,15 +1,31 @@
 import MySQLdb
+import sys
+import csv
+from datetime import date
 
 db = MySQLdb.connect(
     host = 'localhost',
     user = 'jubelmakerspace',
-    passwd = 'MakerTech138'
-    db = 'makerspace_sign_ins'
-    table = 'sign_ins'
+    passwd = 'MakerTech138',
+    database = 'makerspace_sign_ins'
 )
+today = date.today()
+d = today.strftime(%d-%m-%y)
+filename = "signins_" +str(today)+".csv"
+print(filename)
+
+
+QUERY='SELECT * FROM sign_ins;'
+
 
 print(db)
 cur = db.cursor()
+cur.execute(QUERY)
+result = cur.fetchall()
+
+c = csv.writer(open(filename, 'wb'))
+for x in result:
+    c.writerow(x)
 
 
 
