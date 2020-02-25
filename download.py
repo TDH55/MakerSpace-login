56d3ce2b6b01b72b61e17f465ac0dc1e6d4b8270
Taylor Howard <taylorhoward1@wustl.edu>
2020-02-25 17:09:19 -0600

    test write

--- download.py
+++ download.py
@@ -23,10 +23,18 @@ cur = db.cursor()
 cur.execute(QUERY)
 result = cur.fetchall()
 
-c = csv.writer(open(filename, 'wb'))
-for x in result:
-    c.writerow(x)
+# c = csv.writer(open(filename, 'wb'))
+# for x in result:
+#     c.writerow(x)
 
+
+with open('persons.csv', 'wb') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['Name', 'Profession'])
+    filewriter.writerow(['Derek', 'Software Developer'])
+    filewriter.writerow(['Steve', 'Software Developer'])
+    filewriter.writerow(['Paul', 'Manager'])
 
 
 
